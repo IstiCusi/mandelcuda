9bca191603edb80d076da60d827329a5591061ec
Stephan Strauss <StephanStrauss@gmx.ch>
2024-04-09 21:45:05 +0200

    Delete old comment / real API docu to follow

--- mandel_cuda.cu
+++ mandel_cuda.cu
@@ -4,15 +4,10 @@ __device__ float mapIterationsToWavelength(int iterations, int maxIter, float mi
 
     maxIter = maxIter / 50.0f;
 
-    // Verwende den Logarithmus der aktuellen Iteration + 1, um eine Division durch Null zu vermeiden
     float logCurrent = logf(iterations + 1);
-    // Logarithmus der maximalen Iteration + 1 für die Skalierung
     float logMax = logf(maxIter + 1);
-    
-    // Berechne den Skalierungsfaktor auf einer logarithmischen Skala
     float scale = (logCurrent - logf(1)) / (logMax - logf(1));
 
-    // Berechne die Wellenlänge mithilfe des Skalierungsfaktors
     float wavelength = minWavelength + (maxWavelength - minWavelength) * scale;
     return wavelength;
 }
@@ -25,6 +20,7 @@ __global__ void mandelbrotKernel(int *output, float *wavelengths, float xmin, fl
         float x0 = xmin + (float)idx / width * (xmax - xmin);
         float y0 = ymin + (float)idy / height * (ymax - ymin);
         float x = 0.0f, y = 0.0f;
+
         int iteration = 0;
         while (x*x + y*y <= 4 && iteration < maxIter) {
             float xtemp = x*x - y*y + x0;
@@ -55,7 +51,7 @@ extern "C" void computeMandelbrot(int *output         , float *wavelengths     ,
 
     mandelbrotKernel<<<dimGrid, dimBlock>>>(d_output, d_wavelengths, xmin, xmax, ymin, ymax, width, height, maxIter, minWavelength, maxWavelength);
 
-    cudaMemcpy(output, d_output, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(output, d_output, size, cudaMemcpyDeviceToHost); // Blocks automatically
     cudaMemcpy(wavelengths, d_wavelengths, sizeWavelengths, cudaMemcpyDeviceToHost);
     cudaFree(d_output);
     cudaFree(d_wavelengths);
